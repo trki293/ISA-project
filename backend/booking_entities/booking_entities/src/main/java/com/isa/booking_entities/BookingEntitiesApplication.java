4faacad4ab3b72bc476dc63f55b70b96e021454f
trki293 <59773039+trki293@users.noreply.github.com>
2021-12-22 12:19:25 +0100

    feat : Adding an initial backend

---
+++ backend/booking_entities/booking_entities/src/main/java/com/isa/booking_entities/BookingEntitiesApplication.java
@@ -0,0 +1,13 @@
+package com.isa.booking_entities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingEntitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingEntitiesApplication.class, args);
+	}
+
+}
---
+++ backend/booking_entities/booking_entities/src/test/java/com/isa/booking_entities/BookingEntitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isa.booking_entities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingEntitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
